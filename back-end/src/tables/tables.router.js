67abbf2b3d5adc966b9e1072790bf31697744cd5
Victor <victorddaramola@gmail.com>
2022-06-21 21:42:23 -0400

    Created routes for CRUD methods

---
+++ back-end/src/tables/tables.router.js
@@ -0,0 +1,19 @@
+const router = require("express").Router();
+const controller = require("./tables.controller");
+const reservationController = require("../reservations/reservations.controller");
+const methodNotAllowed = require("../errors/methodNotAllowed");
+
+router
+  .route("/")
+  .get(controller.list)
+  .post(controller.create)
+  .all(methodNotAllowed);
+
+router.route("/:table_id").get(controller.read).all(methodNotAllowed);
+
+router
+  .route("/:table_id/seat")
+  .put(reservationController.reservationExists, controller.seat)
+  .all(methodNotAllowed);
+
+module.exports = router;
