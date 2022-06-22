78adbcce6d12f834d6d47e1593abb0c126609bed
Victor <victorddaramola@gmail.com>
2022-06-22 01:03:07 -0400

    Added PUT method to reservation id route

--- back-end/src/reservations/reservations.router.js
+++ back-end/src/reservations/reservations.router.js
@@ -14,7 +14,11 @@ router
   .post(controller.create)
   .all(methodNotAllowed);
 
-router.route("/:reservation_id").get(controller.read).all(methodNotAllowed);
+router
+  .route("/:reservation_id")
+  .get(controller.read)
+  .put(controller.update)
+  .all(methodNotAllowed);
 
 router
   .route("/:reservation_id/status")
